a9bd36473c40da2670a3188ce3ceb01b8773246b
xanniex <105021023@live.asia.edu.tw>
2017-02-24 14:57:57 +0800

    hw submit

---
+++ src/hw00.java
@@ -0,0 +1,15 @@
+/*
+* Topic: 自我介紹
+* Date: 2017/02/24
+* Author: 105021023 林郁憲
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("姓名:林郁憲");
+    System.out.println("學號:105021023");
+    System.out.println("班級:資工系資電組1A");
+	}
+
+}
